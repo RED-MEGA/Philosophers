614979594c281c52c3c8f447deb3f6f5b823f376
Redouane Iben Hamou <reben-ha@e2r6p13.1337.ma>
2023-04-23 21:11:58 +0100

    Add

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: reben-ha <reben-ha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 22:19:02 by reben-ha          #+#    #+#             */
-/*   Updated: 2023/04/23 18:05:13 by reben-ha         ###   ########.fr       */
+/*   Updated: 2023/04/23 20:09:35 by reben-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,8 @@ bool	print_stat(t_philo *philo, char *stat, char *color, int unlock)
 	pthread_mutex_lock(philo->print_access);
 	if (!get_life_state(philo))
 		return (false);
-	printf("%s%lld : Hi I'm philo %d : %s\n", color, (current_time() - philo->t0), philo->id, stat);
+	printf("%s%lld : Hi I'm philo %d : %s\n",
+		color, (current_time() - philo->t0), philo->id, stat);
 	if (unlock)
 		pthread_mutex_unlock(philo->print_access);
 	return (true);
@@ -43,21 +44,17 @@ static void	eating(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->fork);
 	pthread_mutex_lock(&philo->next->fork);
-
 	pthread_mutex_lock(&philo->meal_count.mutex);
 	philo->meal_count.value += 1;
 	pthread_mutex_unlock(&philo->meal_count.mutex);
-
 	pthread_mutex_lock(&philo->last_meal.mutex);
 	philo->last_meal.value = current_time();
 	pthread_mutex_unlock(&philo->last_meal.mutex);
-
 	if (print_stat(philo, "Take fork", C_EAT, true) == false)
 		return ;
 	if (print_stat(philo, "Eating", C_EAT, true) == false)
 		return ;
 	usleep_x(philo->info->time_to_eat);
-
 	pthread_mutex_unlock(&philo->fork);
 	pthread_mutex_unlock(&philo->next->fork);
 }
@@ -67,11 +64,6 @@ static void	sleeping(t_philo *philo)
 	if (print_stat(philo, "sleeping", C_SLEEP, true) == false)
 		return ;
 	usleep_x(philo->info->time_to_sleep);
-}
-
-static void	thinking(t_philo *philo)
-{
-	print_stat(philo, "thinking", C_THINK, true);
 }
 
 void	*routine(void *ptr)
@@ -85,15 +77,12 @@ void	*routine(void *ptr)
 		eating(philo);
 		if (!get_life_state(philo))
 			return (false);
-			
 		sleeping(philo);
 		if (!get_life_state(philo))
 			return (false);
-
-		thinking(philo);
+		print_stat(philo, "thinking", C_THINK, true);
 		if (!get_life_state(philo))
 			return (false);
-
 	}
 	return (NULL);
 }
